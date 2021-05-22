6019ddb5950fce47e53c8ec2a5eb91f27c23883e
ameetp <ameet.porwal@citrix.com>
2021-05-22 23:23:20 +0530

    Add variadic argurments

--- modules/nummanip/calc/calc.go
+++ modules/nummanip/calc/calc.go
@@ -2,6 +2,11 @@ package calc
 
 // returns sums of two integers
 
-func Add(x, y int) int {
-	return x + y
+func Add(number ...int ) int {
+	sum := 0
+	for _, num := range numbers {
+		sum += num
+	}
+
+	return sum
 }
